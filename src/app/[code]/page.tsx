ef23bfd488ee4483df7ae0d80a416145ca1b3114
onemineus <sangeetbanerjee777@gmail.com>
2023-09-03 07:39:00 +0530

    fixed firebase aerror on code page

--- src/app/[code]/page.tsx
+++ src/app/[code]/page.tsx
@@ -12,20 +12,29 @@ const Page = ({ params }: { params: { code: string } }) => {
     const docSnap = await getDoc(docRef);
     setData(docSnap);
   };
-
+  console.log(data?.data()?.date);
+  const originalDate = new Date(data?.data()?.date.toDate());
+  const formattedDate = originalDate.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const [realDate, setRealDate] = useState("");
   useEffect(() => {
     getData();
     setTimeout(() => {
       setIsLoading(false);
     }, 1000);
   }, []);
-
+  useEffect(() => {
+    setRealDate(formattedDate);
+  }, [data]);
   return !data?.exists() && isLoading ? (
-    <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-100">
+    <div className="flex h-screen items-center justify-center bg-zinc-900 text-zinc-100">
       <div className="text-xl capitalize">checking certificate</div>
     </div>
   ) : !data?.exists() && !isLoading ? (
-    <div className="min-h-screen bg-zinc-950 font-mono text-zinc-100">
+    <div className="min-h-screen bg-zinc-900 font-mono text-zinc-100">
       <div className="flex justify-center py-10">
         <LuShieldAlert size={400} color="red" />
       </div>
@@ -36,30 +45,24 @@ const Page = ({ params }: { params: { code: string } }) => {
         <div className="text-center text-xl">{`Achievement: unknown`}</div>
         <div className="text-center text-xl">{`Date: unknown`}</div>
       </div>
-      <div className="text-center text-5xl">Thank you!</div>
     </div>
   ) : data?.exists() ? (
-    <div className="min-h-screen bg-zinc-950 font-mono text-zinc-100">
-      <div className="flex justify-center py-10">
-        <MdOutlineVerifiedUser size={400} color="#00F700" />
+    <div className="flex h-screen flex-col justify-around bg-zinc-900 font-mono text-zinc-100">
+      <div>
+        <div className="flex justify-center py-10">
+          <MdOutlineVerifiedUser size={200} color="#00F700" />
+        </div>
+        <div className="text-center text-4xl">Certificate Verified!</div>
+        <div className="my-10 flex flex-col">
+          <div className="text-center text-xl">{`Name: ${data.data()
+            ?.name}`}</div>
+          <div className="text-center text-xl">{`Pronounce: ${data.data()
+            ?.sex}`}</div>
+          <div className="text-center text-xl">{`Achievement: ${data.data()
+            ?.for}`}</div>
+          <div className="text-center text-xl">{`Date: ${realDate}`}</div>
+        </div>
       </div>
-      <div className="text-center text-5xl">Certificate Verified!</div>
-      <div className="my-10 flex flex-col">
-        <div className="text-center text-xl">{`Name: ${data.data()
-          ?.name}`}</div>
-        <div className="text-center text-xl">{`Pronounce: ${data.data()
-          ?.sex}`}</div>
-        <div className="text-center text-xl">{`Achievement: ${data.data()
-          ?.for}`}</div>
-        <div className="text-center text-xl">{`Date: ${data
-          .data()
-          ?.date.toLocaleDateString("en-US", {
-            day: "numeric",
-            month: "long",
-            year: "numeric",
-          })}`}</div>
-      </div>
-      <div className="text-center text-5xl">Thank you!</div>
     </div>
   ) : (
     <></>
