4fe81765d3f8343f089419dd70d26a292d84bfa3
onemineus <sangeetbanerjee777@gmail.com>
2023-09-03 12:14:55 +0530

    fixed a razorpay function bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,6 +61,54 @@ export default function Home() {
   useEffect(() => {
     setCanDownload(false);
   }, []);
+  const handlePayment = async (amount: string, Razorpay: any) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}api`);
+    const order_id = await response.json();
+    const options = {
+      key: "rzp_test_oFW3klARVMsOoT",
+      amount: amount,
+      currency: "INR",
+      name: "FAKEFICATE",
+      description: "Pay to get the certificate",
+      image:
+        "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+      order_id: order_id.id,
+      handler: function (response: any) {
+        setCanDownload(true);
+        toast.success("Order confirmed!", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          pauseOnFocusLoss: false,
+          theme: "dark",
+        });
+        setTimeout(() => {
+          setCanDownload(false);
+        }, 1000);
+      },
+      // prefill: {
+      //   name,
+      //   email,
+      //   contact,
+      // },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#18181B",
+      },
+    };
+    const rzpay = new Razorpay(options);
+    rzpay.on("payment.failed", function (response: any) {
+      alert(response.error.code);
+    });
+    rzpay.open();
+  };
+
   return (
     <>
       <Container>
